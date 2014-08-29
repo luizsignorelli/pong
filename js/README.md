2b884ff3b03d56b65f1fd3b0f81eda3b598170fd
Luiz Fernando Signorelli <luiz.sg@gmail.com>
2014-08-29 17:25:47 -0300

    readme + instructions

---
+++ js/README.md
@@ -0,0 +1,12 @@
+pong
+====
+
+The classic pong game implemented in javascript for [The Great Code Club](https://github.com/greatcodeclub/pong).
+
+You can play against the computer or a friend.
+
+### Running the game
+
+1. Clone the project;
+2. Execute the `start_server.sh` script (You'll need ruby installed);
+3. Go to http://localhost:3000/game.html
--- js/game.js
+++ js/game.js
@@ -72,42 +72,29 @@ Game.prototype.start = function() {
   })
 }
 
-// Instead of relying on a timer, we use a special browser function called
-// `requestAnimationFrame(callback)`. It calls the `callback` at interval
-// synced with the display refresh rate.
-// More info at:
-// https://developer.mozilla.org/en/docs/Web/API/window.requestAnimationFrame
 var onFrame = function(callback) {
   if (window.requestAnimationFrame) {
     requestAnimationFrame(function() {
       callback()
-      // requestAnimationFrame only calls our callback once, we need to
-      // schedule the next call ourself.
       onFrame(callback)
     })
   } else {
-    // requestAnimationFrame is not supported by all browsers. We fall back to
-    // a timer.
     var fps = 60
     setInterval(callback, 1000 / fps)
   }
 }
 
-// With fixed time steps, each update is done at a fixed interval.
 Game.prototype.fixedTimeStep = function() {
   var fps = 60,
       interval = 1000 / fps,
       updated = false
 
-  // While we're not up to date ...
   while (this.lastUpdateTime < new Date().getTime()) {
     this.update()
     updated = true
-    // We jump at fixed intervals until we catch up to the current time.
     this.lastUpdateTime += interval
   }
 
-  // No need to draw if nothing was updated
   if (updated) this.draw()
   updated = false
 }
